e0c8bb5516c16e608274631357162101369ffe2f
vdbeast <122833284+vdbeast@users.noreply.github.com>
2023-11-29 14:44:40 +0200

    added page files

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,11 @@
+import styles from './HomePage.module.css'
+
+const HomePage = () => {
+    return (
+        <div className={styles.container}>
+            <p>Vova</p>
+        </div>
+    )
+}
+
+export default HomePage;
