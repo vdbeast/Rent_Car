e25149ba65d568d41a37a1c88d5c06972dccc582
vdbeast <122833284+vdbeast@users.noreply.github.com>
2023-11-30 17:20:44 +0200

    added load more btn

--- src/components/CatalogList/CatalogList.jsx
+++ src/components/CatalogList/CatalogList.jsx
@@ -2,17 +2,32 @@ import { useSelector } from "react-redux"
 import CatalogListItem from "../CatalogListItem/CatalogListItem"
 import { selectCatalog } from "../../redux/selectors"
 import styles from './CatalogList.module.css'
+import { useState } from "react"
 
 const CatalogList = () => {
     const cars = useSelector(selectCatalog);
+    const limit = 12;
+    const [visibleCars, setVisibleCars] = useState(limit);
+
+    const handleLoadMore = () => {
+        setVisibleCars((prevVisibleCars)=> prevVisibleCars + limit)
+    }
 
     return (
         <div className={styles.container}>
             <ul className={styles.list}>
-                {cars.map((car) => (
+                {cars.slice(0, visibleCars).map((car) => (
                     <CatalogListItem key={car.id} car={car} />  
                 ))}
             </ul>
+            {visibleCars < cars.length && (
+                <button
+                    className={styles.load_more_btn}
+                    onClick={handleLoadMore}
+                >
+                    Load More
+                </button>
+            )}
         </div>
     )
 }
